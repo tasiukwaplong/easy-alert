0e5a9dfb3ed8280fb50e2f266ee580523f82f075
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-04-30 12:43:42 -0700

    deleted unnecessary files

--- src/views/includes/Home-sender.vue
+++ src/views/includes/Home-sender.vue
@@ -33,9 +33,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div
-                        class="text-xs font-weight-bold text-primary text-uppercase mb-1"
-                      >Account type</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Account type</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800">{{userdata.message.type}}</div>
                     </div>
                     <div class="col-auto">
