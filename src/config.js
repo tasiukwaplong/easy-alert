43b565b7c033bd563250f0adbff774b2cc2c44cd
TK <tasiukwaplong@gmail.com>
2020-04-30 18:34:29 +0100

    Changed endpoint url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: 'http://localhost:5000',
+    API_URL: 'https://trending-in-naija.uc.r.appspot.com/',
     USER_LOGIN: 'login_user_easy_alert'
 };
 module.exports = CONFIG ;
